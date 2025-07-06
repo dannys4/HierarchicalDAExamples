c2d0e1ed44bf5c9c380a1aa5184e34521dc1c300
Daniel Sharp <dannys4@mit.edu>
2025-07-06 15:44:45 -0400

    More complicated slurm example

--- cluster/ex_submit.sh
+++ cluster/ex_submit.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# Example slurm submission script for MIT engaging.
+# partition: mit_normal, print logs to `./log` subdir, 12 jobs (do the same thing), 16 cores per job, 10GB RAM minimum.
 #SBATCH -p mit_normal
 #SBATCH -o log/burgers.log-%A-%a
-#SBATCH -N 1
+#SBATCH -n 16
 #SBATCH -a 1-12
 #SBATCH --mem=10G
 
-# Example slurm submission script
-# echo $(nproc)
-./run_example burgers -n 2
+# Run the burgers example two time for three different noise levels and four ensemble sizes (2*3*4=24 trials)
+# Save the data to the pool directory to allow for significant storage, and only simulate to time 0.5 for each trial.
+./run_example burgers -n 2 -- data_path=/pool001/$USER/ tf=0.5 sigma_x_filter=1e-3,1e-2,1e-1, Ne=25,50,75,100
